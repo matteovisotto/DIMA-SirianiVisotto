7b5545836443415707f2cc4c2860ecde6cec328a
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-04-04 08:17:28 +0200

    Update parser

--- Scraper/Sources/Scraper/AmazonScraper.swift
+++ Scraper/Sources/Scraper/AmazonScraper.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftSoup
 
-struct AmazonProduct {
+struct AmazonProduct: Codable {
     var name: String
     var price: String
     var description: String
@@ -17,8 +17,29 @@ struct AmazonProduct {
 
 class AmazonHTMLParser {
     
-    public static func parseString(str: String, completionHandler: @escaping (_ product: AmazonProduct) -> ()){
-        
+    public static func parseString(str: String, completionHandler: @escaping (_ product: AmazonProduct?) -> ()){
+        do {
+           let doc: Document = try SwiftSoup.parse(str)
+            let name = try doc.select("span#productTitle").first()?.text()
+            let images = try doc.select("#altImages li.item")
+            var imgArray: [String] = []
+            for image in images {
+                let url = try image.select("img").attr("src").replacingOccurrences(of: "US40", with: "SR320,320")
+                imgArray.append(url)
+            }
+            
+            let priceWhole = try doc.select("span.a-price-whole").first?.text().replacingOccurrences(of: ",", with: "")
+            let priceDecimal = try doc.select("span.a-price-fraction").first?.text()
+            let priceSymbol = try doc.select("span.a-price-symbol").first?.text()
+            guard let pName = name, let pI = priceWhole, let pD = priceDecimal, let pS = priceSymbol else {completionHandler(nil); return}
+            
+            let product = AmazonProduct(name: pName, price: pI+"."+pD+pS, description: "", images: imgArray)
+            
+            completionHandler(product)
+            
+        } catch {
+            completionHandler(nil)
+        }
     }
 }
 
@@ -31,24 +52,23 @@ class AmazonScraper {
     init(urlString: String, priceOnly: Bool, completionHandler: @escaping (_ printable: String) -> ()) {
         self.completionHandler = completionHandler
         self.priceOnly = priceOnly
-        let downloader: DownloadManager = DownloadManager(urlString: urlString)
-        downloader.delegate = self
-        downloader.execute()
-    }
-    
-    
-}
-
-
-extension AmazonScraper: DownloadManagerDelegate {
-    func downloadManager(_ downloadManager: DownloadManager, didFinishWithResult result: Bool, contentString: String) {
-        if result {
+        
+        guard let url = URL(string: urlString) else {completionHandler("{\"exception\":\"Invalid URL\"}"); return}
+       
+        do {
+            let contentString = try String(contentsOf: url, encoding: .utf8)
             AmazonHTMLParser.parseString(str: contentString) { product in
-                
+                do{
+                    let jsonEncoder = JSONEncoder()
+                    let jsonData = try jsonEncoder.encode(product)
+                    guard let jsonString = String(data: jsonData, encoding: String.Encoding.utf8) else {completionHandler("{\"exception\":\"Unable to convert json\"}"); return}
+                    completionHandler(jsonString)
+                }catch{
+                    completionHandler("{\"exception\":\"Unable to convert json\"}")
+                }
             }
-        } else {
-            completionHandler("{\"exception\":\""+contentString+"\"}")
+        } catch {
+            completionHandler("{\"exception\":\"An error occured while parsing content\"}")
         }
-        exit(0)
     }
 }
