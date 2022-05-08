dbbc732836ccf6ed4d70208b070b31e798c2e868
TiaSirio <mattia.siriani@mail.polimi.it>
2022-05-08 20:33:50 +0200

    Charts.

--- iOSApp/myAPTracker/ProductView/PriceView.swift
+++ iOSApp/myAPTracker/ProductView/PriceView.swift
@@ -6,16 +6,31 @@
 //
 
 import SwiftUI
-import LineChartView
+//import LineChartView
+import SwiftUICharts
 
 struct PriceView: View {
     
     @ObservedObject var viewModel: ProductViewModel
+    var chartStyle = ChartStyle(backgroundColor: Color("BackgroundColor"), accentColor: Color.blue, gradientColor: GradientColor(start: Color.red, end: Color.red), textColor: Color("LabelColor"), legendTextColor: Color("LabelColor"), dropShadowColor: Color("BackgroundColor"))
+    var chartDarkStyle = ChartStyle(backgroundColor: Color("BackgroundColor"), accentColor: Color.blue, gradientColor: GradientColor(start: Color.orange, end: Color.orange), textColor: Color("LabelColor"), legendTextColor: Color("LabelColor"), dropShadowColor: Color("BackgroundColor"))
+    
+    init(viewModel: ProductViewModel) {
+        self.viewModel = viewModel
+        chartStyle.darkModeStyle = chartDarkStyle
+    }
     
     var body: some View {
         VStack{
             HLPriceView(lowestPrice: viewModel.product.lowestPrice, highestPrice: viewModel.product.highestPrice).padding(.horizontal)
-            LineChartView(lineChartParameters: LineChartParameters(data: viewModel.productPrices)).frame(height: 160)
+            //La size è definita da una CGSize quindi si può gestire bene
+            LineChartView(data: viewModel.productPrices, title: "Trend", style: chartStyle, form: ChartForm.extraLarge, dropShadow: false, valueSpecifier: "%.2f")
+            /*LineChart()
+                .data(viewModel.productPrices)
+                .chartStyle(ChartStyle(backgroundColor: Color("BackgroundColor"),
+                                       foregroundColor: ColorGradient(.blue, .accentColor)))
+                .frame(height: 200)*/
+            //LineChartView(lineChartParameters: LineChartParameters(data: viewModel.productPrices)).frame(height: 160)
             ScrollView(.vertical, showsIndicators: false){
                 VStack(spacing: 2){
                     ForEach(0..<(viewModel.product.prices?.count ?? 0), id: \.self){index in
