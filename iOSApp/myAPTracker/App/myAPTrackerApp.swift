686c09d1b3f7fe7dc79f7524d00cc1a16e3dbedc
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-06-25 22:10:01 +0200

    iPad changes

--- iOSApp/myAPTracker/App/myAPTrackerApp.swift
+++ iOSApp/myAPTracker/App/myAPTrackerApp.swift
@@ -12,23 +12,7 @@ struct myAPTrackerApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
-            if UIDevice.current.userInterfaceIdiom == .pad {
-                iPadContentView()
-                    .environmentObject(AppState.shared)
-                    .onAppear {
-                    if let rootVC = UIApplication.shared.windows.first?.rootViewController {
-                        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: rootVC, action: #selector(rootVC.dismissKeyboard))
-                        tap.cancelsTouchesInView = false
-                        rootVC.view.addGestureRecognizer(tap)
-                    }
-                    }.onOpenURL { url in
-                        let str = url.absoluteString
-                        if str.contains("product?id") {
-                            let id = str.split(separator: "=")[1]
-                            AppDelegate.shared?.parseProduct(productId: String(id))
-                        }
-                    }
-            } else {
+            if UIDevice.current.userInterfaceIdiom == .phone {
                 ContentView()
                     .environmentObject(AppState.shared)
                     .onAppear {
@@ -44,6 +28,23 @@ struct myAPTrackerApp: App {
                             AppDelegate.shared?.parseProduct(productId: String(id))
                         }
                     }
+            } else {
+                iPadContentView()
+                    .environmentObject(AppState.shared)
+                    .onAppear {
+                    if let rootVC = UIApplication.shared.windows.first?.rootViewController {
+                        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: rootVC, action: #selector(rootVC.dismissKeyboard))
+                        tap.cancelsTouchesInView = false
+                        rootVC.view.addGestureRecognizer(tap)
+                    }
+                    }.onOpenURL { url in
+                        let str = url.absoluteString
+                        if str.contains("product?id") {
+                            let id = str.split(separator: "=")[1]
+                            AppDelegate.shared?.parseProduct(productId: String(id))
+                        }
+                    }
+                
             }
             
         }
--- iOSApp/myAPTracker/MainView/iPadMainView.swift
+++ iOSApp/myAPTracker/MainView/iPadMainView.swift
@@ -8,30 +8,50 @@
 import SwiftUI
 
 struct iPadMainView: View {
+    @EnvironmentObject var appState: AppState
+    @ObservedObject var viewModel: MainViewModel = MainViewModel()
+    
     init(){
         UITableView.appearance().backgroundColor = UIColor(named: "BackgroundColor")?.withAlphaComponent(0.6) // background color of list
-       
+        UINavigationBar.appearance().tintColor = UIColor(named: "PrimaryLabel")!
     }
     var body: some View {
         ZStack{
             Color("BackgroundColor").ignoresSafeArea()
             NavigationView{
-                List{
-                    NavigationLink{
-                        ZStack{
-                            Color("BackgroundColor").ignoresSafeArea()
-                            Text("A")
-                        }
-                    } label: {
-                        Text("Link A")
+                ZStack{
+                    Color("BackgroundColor").ignoresSafeArea()
+                    VStack{
+                        Text("myAPTracker").foregroundColor(Color("PrimaryLabel"))
+                        List{
+                            ForEach(0 ..< MainViewModel.tabs.count, id:\.self) { index in
+                                NavigationLink{
+                                    switch MainViewModel.tabs[index].tag {
+                                    case 0:
+                                        HomeView(mainViewModel: viewModel).navigationTitle(MainViewModel.tabs[index].tabName)
+                                    case 1:
+                                        TrackedView(showLogin: $viewModel.showLogin).navigationTitle(MainViewModel.tabs[index].tabName)
+                                    case 2:
+                                        ExploreView().navigationTitle(MainViewModel.tabs[index].tabName)
+                                    case 3:
+                                        SettingView(showLogin: $viewModel.showLogin).navigationTitle(MainViewModel.tabs[index].tabName)
+                                    default:
+                                        EmptyView()
+                                    }
+                                } label: {
+                                    HStack{
+                                        Image(systemName: MainViewModel.tabs[index].iconSystemName)
+                                        Text(MainViewModel.tabs[index].tabName)
+                                    }
+                                    
+                                }
+                            }
+                        }.listStyle(.sidebar).foregroundColor(Color("PrimaryLabel"))
                     }
-                    NavigationLink{
-                        Text("B")
-                    } label: {
-                        Text("Link B")
-                    }
-                }.listStyle(.sidebar).foregroundColor(Color("PrimaryLabel"))
-            }
+                }
+                }
+        }.sheet(isPresented: $viewModel.showLogin) {
+            LoginView($viewModel.showLogin).padding()
         }
     }
 }
