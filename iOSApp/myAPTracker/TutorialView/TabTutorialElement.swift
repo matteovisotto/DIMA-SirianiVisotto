023ddec0d2e4b9873b04749044610a5db99bf4f8
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-06-28 22:38:16 +0200

    Localization IT

--- iOSApp/myAPTracker/TutorialView/TabTutorialElement.swift
+++ iOSApp/myAPTracker/TutorialView/TabTutorialElement.swift
@@ -19,8 +19,8 @@ struct TabTutorialElement: Identifiable {
 }
 
 var tabTutorialElement: [TabTutorialElement] = [
-    TabTutorialElement(id: UUID().uuidString, title: "myAPTracker", subtitle: "Welcome in", description: "Keep track of your favourite Amazon product", image: "hand.thumbsup", color: Color("Tutorial1"), fontColor: Color("Tutorial3")),
-    TabTutorialElement(title: "Stay update", subtitle: "Create your account", description: "Create an account or sign in via socials", image: "waveform.path.ecg", color: Color("Tutorial2"), fontColor: Color("Tutorial3")),
-    TabTutorialElement(title: "Get notified", subtitle: "Track every product", description: "Get notified whenever your tracked product has a lower price", image: "bell.fill", color: Color("Tutorial3"), fontColor: Color("Tutorial1"))
+    TabTutorialElement(id: UUID().uuidString, title: "myAPTracker", subtitle: NSLocalizedString("Welcome in", comment: "Welcome in"), description: NSLocalizedString("Keep track of your favourite Amazon product", comment: "Keep track of your favourite Amazon product"), image: "hand.thumbsup", color: Color("Tutorial1"), fontColor: Color("Tutorial3")),
+    TabTutorialElement(title: NSLocalizedString("Stay update", comment: "Stay update"), subtitle: NSLocalizedString("Create your account", comment: "Create your account"), description: NSLocalizedString("Create an account or sign in via socials", comment: "Create an account or sign in via socials"), image: "waveform.path.ecg", color: Color("Tutorial2"), fontColor: Color("Tutorial3")),
+    TabTutorialElement(title: NSLocalizedString("Get notified", comment: "Get notified"), subtitle: NSLocalizedString("Track every product", comment: "Track every product"), description: NSLocalizedString("Get notified whenever your tracked product has a lower price", comment: "Get notified whenever your tracked product has a lower price"), image: "bell.fill", color: Color("Tutorial3"), fontColor: Color("Tutorial1"))
 ]
 
